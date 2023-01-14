c466428fc1b8b082abf3be2f61d84cdfaaab244b
Nikita Balagansky <Balaganskij.NN@phystech.edu>
2023-01-15 00:36:01 +0300

    start without systemd

---
+++ task1/bin/setup.sh
@@ -0,0 +1,4 @@
+mkdir app 
+cd app && git clone https://github.com/linkedin/oncall.git
+cd /app/oncall && pip install -e . ".[dev]"
+
---
+++ task1/bin/start.sh
@@ -0,0 +1,7 @@
+service mysql start
+sleep 3
+mysql -u root < ./db/schema.v0.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234'; FLUSH PRIVILEGES;" | mysql -u root
+
+
+oncall-dev /app/oncall/configs/config.yaml
